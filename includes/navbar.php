3ab9e3d642a059387482e6f0de5687b2873e6908
Gh0zT <riewartvlaarhoven@gmail.com>
2016-09-17 00:32:51 +0200

    added logintype

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,11 +2,9 @@
     <div class="wrapper">
         <h1 id="logo">BcTutor</h1>
         <ul>
-            <li><p>Bijles geven</p></li><!--
-
-            --><li><p>Bijles krijgen</p></li><!--
-
-            --><li><p>Instellingen</p></li>
+            <li><p>Bijles geven</p></li>
+            <li><p>Bijles krijgen</p></li>
+            <li><p>Instellingen</p></li>
         </ul>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" type="text/css" href="style/style.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <meta charset="UTF-8">
+        
+        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js?ver=1.4.2'></script>
     </head>
     
     <body>
@@ -14,24 +16,57 @@
             <div id="login-box">
                 <h2 class="login-caption">Login</h2>
 
-                <?php include 'includes/navbar.php'; ?>
-
-                <form action="login-magister.php" class="login-form">
+                <form id="magister-login" action="login-magister.php" class="login-form">
                     <p>Log in met je magister leerlingnummer</p>
 
-                    <input autofocus="autofocus" name="magisteruser" type="text" placeholder="Leerlingnummer">
+                    <input autofocus="autofocus" name="magisteruser" type="text" placeholder="leerlingnummer">
                     <input name="magisterpassword" type="password" placeholder="wachtwoord">
 
-                    <input checked="checked" type="checkbox" id="remember" name="remember_me">
+                    <input checked="checked" type="checkbox" id="rememberme" name="remember_me">
                     <label for="remember">Mijn gegevens onthouden</label>
 
                     <button class="margin-reset right" type="submit">Inloggen</button>
+                </form>
+                
+                <form id="normal-login" action="login.php" class="login-form">
+                    <p>Log in met je BcTutor account</p>
 
+                    <input autofocus="autofocus" name="username" type="text" placeholder="gebruikersnaam / e-mailadres">
+                    <input name="password" type="password" placeholder="wachtwoord">
+
+                    <input checked="checked" type="checkbox" id="rememberme" name="remember_me">
+                    <label for="remember">Mijn gegevens onthouden</label>
+
+                    <button class="margin-reset right" type="submit">Inloggen</button>
                 </form>
 
                 <div class="divider"></div>
-
-                <button type="submit">Log in met je BcTutor account</button>
+                
+                <button type="submit" id="change-logintype">Log in met je BcTutor account</button>
+                
+                <script>
+                    $(document).ready(function(){
+                        $("#normal-login").hide();
+                    });
+                    
+                    $a = 1
+                    
+                    $("#change-logintype").click(function(){
+                        if ($a == 1) {
+                            $("#magister-login").fadeOut(500);
+                            setTimeout(function() {
+                                $("#normal-login").fadeIn(500);
+                            }, 500);
+                            $a = 2;
+                        } else {
+                            setTimeout(function() {
+                                $("#magister-login").fadeIn(500);
+                            }, 500);
+                            $("#normal-login").fadeOut(500);
+                            $a = 1;
+                        }
+                    });
+                </script>
             </div>
         </div>
     </body>
